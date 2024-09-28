d21a0ad26380e9868d5aec9a369907c772b9e20f
Gabriel Araujo <ga.padua92@gmail.com>
2024-09-28 09:17:49 -0300

    (28/09) Continuação da implementação de SQL Injector

--- vulcom/src/controllers/sql-injection.js
+++ vulcom/src/controllers/sql-injection.js
@@ -1,4 +1,8 @@
+import sql from "../database/db"
+
+
 const controller = {}
+
 controller.login = function(req, res) {
   res.render('sql-injection/login', {
     title: 'Autentique-se',
@@ -7,4 +11,53 @@ controller.login = function(req, res) {
     message: ''
   })
 }
+
+controller.processLogin = async function(req, res) {
+    try{
+        //usado concatenação de valores em SQL de forma insegura para reproduzir SQL Injection
+        const result = await ([sql`SELECT * FROM users WHERE username = '${req.body.username} AND password = ${req.body.password}'`])
+        console.log(result)
+        if(result){
+            res.render('sql-injection/login', {
+                title: 'Autenticado'
+            })
+        } else {
+            res.render('sql-injection/login', {
+                title: 'Autentique-se',
+                username: req.body.username,
+                password: req.body.password,
+                message: 'Usuário ou senha inválidos'
+            })
+        }
+    } catch(error){
+        console.error(error)
+        res.render('sql-injection/login', {
+            title: 'Autentique-se',
+            username: req.body.username,
+            password: req.body.password,
+            message: error.message
+          })
+    }
+}
+
+//   const { username, password } = req.body
+//   const query = `SELECT * FROM users WHERE username = '${username}' AND password = '${password}'`
+//   const users = await sql`
+//     ${query}
+//   `
+//   if (users.length === 0) {
+//     res.render('sql-injection/login', {
+//       title: 'Autentique-se',
+//       username,
+//       password,
+//       message: 'Usuário ou senha inválidos'
+//     })
+//   } else {
+//     res.render('sql-injection/welcome', {
+//       title: 'Bem-vindo',
+//       username
+//     })
+//   }
+// }
+
 export default controller
