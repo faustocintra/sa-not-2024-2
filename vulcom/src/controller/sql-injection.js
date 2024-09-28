da87a70a047e49887df94f01959b2ec3f60dddc1
JOAO VICTOR GARCIA <1091392223009@fatec-srv.local>
2024-09-28 09:12:04 -0300

    (28/09) COntinuação da implementação de  SQL injection

---
+++ vulcom/src/controller/sql-injection.js
@@ -0,0 +1,42 @@
+import sql from '../database/db.js'
+
+const controller = {}
+
+controller.login = function(req, res) {
+    res.render('sql-injection/login', {
+        title: 'Autentique-se',
+        username: '',
+        password: '',
+        message: ''
+    })
+}
+
+
+controller.processoLogin = async function(req, res) {
+    try {
+        //Usando concatenaçãi de valores sql de forma insegura para reproduzir sql injection
+        const result = await sql([
+            `select * from users where username = '${username}'
+            and password = '${req.body.password}`
+        ])
+
+        if(result) res.render('sql-injection/success', { title: 'Autenticado'})
+        else res.render('sql-injection/login', {
+            title: 'Autentique-se',
+            username: req.body.username,
+            password: req.body.password,
+            message: 'Usuario ou senha invalidos'
+        })
+    }
+    catch(error) {
+        console.error(error)
+        res.render('sql-injection/login', {
+            title: 'Autentique-se',
+            username: req.body.username,
+            password: req.body.password,
+            message: 'Usuario ou senha invalidos'
+        })
+    }
+}
+
+export default controller
---
+++ vulcom/src/controllers/sql-injection.js
@@ -0,0 +1,12 @@
+const controller = {}
+
+controller.login = function(req, res) {
+  res.render('sql-injection/login', {
+    title: 'Autentique-se',
+    username: '',
+    password: '',
+    message: ''
+  })
+}
+
+export default controller
