0ccb12aee2a5aa5a327b766e6b73ee55b343afc3
Claudio Matos <claudiomatos@gmail.com>
2024-09-28 09:14:46 -0300

    (28/09) Continuação da implementação de SQL Injection

--- vulcom/src/routes/index.js
+++ vulcom/src/routes/index.js
@@ -3,7 +3,7 @@ const router = Router()
 
 /* GET home page. */
 router.get('/', function (req, res) {
-  res.render('index', { title: 'VULCOM' })
+  res.render('index', { title: 'Vulcom' })
 })
 
 export default router
