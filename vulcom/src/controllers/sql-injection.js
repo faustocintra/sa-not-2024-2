f84cce6668d9d90a086b35381969f2f548ce03b1
RaulSAraujo <raul.araujo.2000@hotmail.com>
2024-10-02 21:40:13 -0300

    (02/10) Término da fase 1 do SQL Injection

--- vulcom/src/controllers/sql-injection.js
+++ vulcom/src/controllers/sql-injection.js
@@ -1,4 +1,4 @@
-import sql from '../database/db'
+import conn from '../database/db.js'
 
 const controller = {}
 
@@ -15,15 +15,19 @@ controller.processLogin = async function (req, res) {
   try {
     // Usando concatenação de valores em SQL de forma
     // insegura para reproduzir SQL injection
-    const result = await sql([
-      `select * from users where username = '${req.body.username}'
-        and password = '${req.body.password}'`
-    ])
+    const sql = `select * from users where username = '${req.body.username}' and password = '${req.body.password}'`
 
-    if (result) {
-      res.render('sql-injection/success', {title: 'Authenticado'})
+
+    console.log('-'.repeat(80))
+    console.log(sql)
+    console.log('-'.repeat(80))
+
+    const result = await conn.query(sql)
+
+    if (result.rowCount > 0) {
+      res.render('sql-injection/success', { title: 'Autenticado' })
     } else {
-      res.render('sql-injection/login',{
+      res.render('sql-injection/login', {
         title: 'Autentique-se',
         username: req.body.username,
         password: req.body.password,
@@ -34,7 +38,7 @@ controller.processLogin = async function (req, res) {
   catch (error) {
     console.error(error)
 
-    res.render('sql-injection/login',{
+    res.render('sql-injection/login', {
       title: 'Autentique-se',
       username: req.body.username,
       password: req.body.password,
