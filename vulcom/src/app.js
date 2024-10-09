62f2e19ee0c2492c0c5c7729fe27f132aad4f376
Felipe Carvalho Tardivo <fetardivo@gmail.com>
2024-10-09 20:34:14 -0300

    (09/10) Término de SQL Injection e Início de XSS

--- vulcom/src/app.js
+++ vulcom/src/app.js
@@ -1,13 +1,11 @@
 import 'dotenv/config'
+
 import express, { json, urlencoded } from 'express'
 import createError from 'http-errors'
 import cookieParser from 'cookie-parser'
 import logger from 'morgan'
 import { join, dirname } from 'path'
 import { fileURLToPath } from 'url'
-
-//import dotenv from 'dotenv'
-//dotenv.config() //Carrega as variáveis de ambiente do arquivo .env
 
 import indexRouter from './routes/index.js'
 //import usersRouter from './routes/users.js'
@@ -34,6 +32,9 @@ app.use('/', indexRouter)
 
 import sqlInjectionRouter from './routes/sql-injection.js'
 app.use('/sql-injection', sqlInjectionRouter)
+
+import xssRouter from './routes/xss.js'
+app.use('/xss', xssRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- vulcom/src/database/db.js
+++ vulcom/src/database/db.js
@@ -1,7 +1,6 @@
 import pg from 'pg'
 const { Pool } = pg
 
-const conn = new Pool ({ connectionString: process.env.DATABASE_URL })
-
+const conn = new Pool({ connectionString: process.env.DATABASE_URL })
 
 export default conn
