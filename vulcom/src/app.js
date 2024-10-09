eb9c65830e7348dc3185915ffc4032450318e03d
LEONARDO HENRIQUE BERNARDES DE SOUZA <1091392223034@fatec-srv.local>
2024-10-09 20:26:48 -0300

    (09/10) Término de SQL Injection e início de XSS

--- vulcom/src/app.js
+++ vulcom/src/app.js
@@ -34,6 +34,9 @@ app.use('/', indexRouter)
 import sqlInjectionRouter from './routes/sql-injection.js'
 app.use('/sql-injection', sqlInjectionRouter)
 
+import xssRouter from '.router/xss.js'
+app.use('/xss', xssRouter)
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404))
--- vulcom/src/controllers/sql-injection.js
+++ vulcom/src/controllers/sql-injection.js
@@ -47,4 +47,42 @@ controller.processLogin = async function(req, res) {
   }
 }
 
+
+controller.processLogin = async function(req, res) {
+  try{
+    // Usando concatenação de valores em SQL de forma
+    // insegura para reproduzir SQL Injection
+
+    const sql = `select * from users where username = $1 and password = $2`
+    const params = [req.body]
+
+    console.log('_')
+    console.log
+    console.log
+
+
+    const result = await sql([
+      `select * from users where username = '${req.body.username}'
+      and password = '${req.body.password}'`
+    ])
+
+    if(result) res.render('sql-injection/success', { title: 'Autenticado' })
+    else res.render('sql-injection/login', {
+      title: 'Autentique-se',
+      username: req.body.username,
+      password: req.body.password,
+      message: 'Usuário ou senha inválidos'
+    })
+  }
+  catch(error) {
+    console.error(error)
+    res.render('sql-injection/login', {
+      title: 'Autentique-se',
+      username: req.body.username,
+      password: req.body.password,
+      message: error.message
+    })
+  }
+}
+
 export default controller
