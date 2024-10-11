7a6173a944c5073201f17365e5c0ad37407b3c74
Victormoroo <victorhugo0609@outlook.com>
2024-10-10 21:21:15 -0300

    (09/10) Término de SQL Injection e início de XSS

--- vulcom/src/app.js
+++ vulcom/src/app.js
@@ -1,45 +1,54 @@
 import 'dotenv/config'
 
-import express, { json, urlencoded } from "express"
-import createError from "http-errors"
-import cookieParser from "cookie-parser"
-import logger from "morgan"
-import { join, dirname } from "path"
-import { fileURLToPath } from "url"
+import express, { json, urlencoded } from 'express'
+import createError from 'http-errors'
+import cookieParser from 'cookie-parser'
+import logger from 'morgan'
+import { join, dirname } from 'path'
+import { fileURLToPath } from 'url'
 
-import indexRouter from "./routes/index.js"
-//import usersRouter from "./routes/users.js"
+import indexRouter from './routes/index.js'
+//import usersRouter from './routes/users.js'
+
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
+
 const app = express()
+
 // view engine setup
-app.set("views", join(__dirname, "views"))
-app.set("view engine", "ejs")
-app.use(logger("dev"))
+app.set('views', join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+app.use(logger('dev'))
 app.use(json())
 app.use(urlencoded({ extended: false }))
 app.use(cookieParser())
-app.use(express.static(join(__dirname, "../public")))
+app.use(express.static(join(__dirname, '../public')))
 
 /***************** ROTAS ****************************/
 
-app.use("/", indexRouter)
-//app.use("/users", usersRouter)
+app.use('/', indexRouter)
+//app.use('/users', usersRouter)
 
 import sqlInjectionRouter from './routes/sql-injection.js'
 app.use('/sql-injection', sqlInjectionRouter)
 
+import xssRouter from './routes/xss.js'
+app.use('/xss', xssRouter)
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404))
 })
+
 // error handler
 app.use(function (err, req, res) {
   // set locals, only providing error in development
   res.locals.message = err.message
-  res.locals.error = req.app.get("env") === "development" ? err : {}
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
+
   // render the error page
   res.status(err.status || 500)
-  res.render("error")
+  res.render('error')
 })
+
 export default app
