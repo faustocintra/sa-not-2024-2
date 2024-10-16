d3b86e0743e7d45f2cb15d9aefb7d3c24e357dea
Achette <i.achette@live.com>
2024-10-16 20:25:26 -0300

    (16/10) Término de XSS e inicio do cadastro de usuários

---
+++ vulcom/prisma/migrations/20241016231640_alter_users/migration.sql
@@ -0,0 +1,19 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[username]` on the table `users` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[email]` on the table `users` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `email` to the `users` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `fullname` to the `users` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "users" ADD COLUMN     "email" TEXT NOT NULL,
+ADD COLUMN     "fullname" TEXT NOT NULL,
+ADD COLUMN     "is_admin" BOOLEAN NOT NULL DEFAULT false;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_username_key" ON "users"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
--- vulcom/src/controllers/xss.js
+++ vulcom/src/controllers/xss.js
@@ -1,46 +1,51 @@
-import conn from '../database/db.js'
+import conn from "../database/db.js";
 
-const controller = {}
+const controller = {};
 
-controller.index = async function(req, res) {
+controller.index = async function (req, res) {
   try {
-    const sql = 'select * from comments order by date_time desc'
+    const sql = "select * from comments order by date_time desc";
 
-    const result = await conn.query(sql)
+    const result = await conn.query(sql);
 
-    res.render('xss/comments', {
-      title: 'XSS (Cross-Site Scripting)',
-      error: '',
-      comments: result.rows
-    })
-  }
-  catch(error) {
-    console.error(error)
-    res.render('xss/comments', {
-      title: 'XSS (Cross-Site Scripting)',
+    res.render("xss/comments", {
+      title: "XSS (Cross-Site Scripting)",
+      error: "",
+      comments: result.rows,
+    });
+  } catch (error) {
+    console.error(error);
+    res.render("xss/comments", {
+      title: "XSS (Cross-Site Scripting)",
       error: error.message,
-      comments: []
-    })
+      comments: [],
+    });
   }
-}
+};
 
-controller.create = async function(req, res) {
+controller.create = async function (req, res) {
   try {
-    let sql = 'insert into comments (comment) values ($1)'
-    const params = [req.body.comment]
+    /* 
+    Sanitização simples da entrada do usuário para evitar XSS
+    Troca todas as ocorrencias de '<'por '&lt;
+    */
 
-    await conn.query(sql, params)
+    req.body.comment = req.body.comment.replaceAll('<', '&lt;')
+    
+    let sql = "insert into comments (comment) values ($1)";
+    const params = [req.sanitize(req.body.comment)];
 
-    await controller.index(req, res)
-  }
-  catch(error) {
-    console.error(error)
-    res.render('xss/comments', {
-      title: 'XSS (Cross-Site Scripting)',
+    await conn.query(sql, params);
+
+    await controller.index(req, res);
+  } catch (error) {
+    console.error(error);
+    res.render("xss/comments", {
+      title: "XSS (Cross-Site Scripting)",
       error: error.message,
-      comments: []
-    })
+      comments: [],
+    });
   }
-}
+};
 
-export default controller
+export default controller;
