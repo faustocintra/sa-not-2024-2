7b9d5e97219cbb59d4a8f318a7f22ce27b11d0f9
João Pedro <joaoofficialpedro@gmail.com>
2024-10-16 19:25:06 -0300

    Feat: Término SQL Injection e Início de XSS

--- vulcom/src/controllers/sql-injection.js
+++ vulcom/src/controllers/sql-injection.js
@@ -1,4 +1,4 @@
-import sql from '../database/db.js'
+import conn from '../database/db.js'
 
 const controller = {}
 
@@ -11,16 +11,58 @@ controller.login = function(req, res) {
   })
 }
 
-controller.processLogin = async function(req, res) {
+controller.processLoginSqlInjection = async function(req, res) {
   try{
+
     // Usando concatenação de valores em SQL de forma
     // insegura para reproduzir SQL Injection
-    const result = await sql([
-      `select * from users where username = '${req.body.username}'
-      and password = '${req.body.password}'`
-    ])
+    // Para produzir uma injeção de SQL que apaga a tabela 'users',
+    // basta informar a string abaixo no campo 'username':
+    // ' or 1=1; drop table users; --
+    const sql = `select * from users where username = '${req.body.username}' and password = '${req.body.password}'`
 
-    if(result) res.render('sql-injection/success', { title: 'Autenticado' })
+    console.log('-'.repeat(80))
+    console.log(sql)
+    console.log('-'.repeat(80))
+    
+    const result = await conn.query(sql)
+
+    if(result.rowCount > 0) res.render('sql-injection/success', { title: 'Autenticado' })
+    else res.render('sql-injection/login', {
+      title: 'Autentique-se',
+      username: req.body.username,
+      password: req.body.password,
+      message: 'Usuário ou senha inválidos'
+    })
+  }
+  catch(error) {
+    console.error(error)
+    res.render('sql-injection/login', {
+      title: 'Autentique-se',
+      username: req.body.username,
+      password: req.body.password,
+      message: error.message
+    })
+  }
+}
+
+controller.processLogin = async function(req, res) {
+  try{
+
+    // Para evitar SQL Injection, os valores de 'username' e 'password'
+    // serão passados à conexão com o banco de dados como parâmetros
+    const sql = `select * from users where username = $1 and password = $2`
+    const params = [req.body.username, req.body.password]
+
+    console.log('-'.repeat(80))
+    console.log(sql)
+    console.log('-'.repeat(80))
+    
+    // Consulta é feita mesclando o texto do SQL com os parâmetros, de
+    // forma segura
+    const result = await conn.query(sql, params)
+
+    if(result.rowCount > 0) res.render('sql-injection/success', { title: 'Autenticado' })
     else res.render('sql-injection/login', {
       title: 'Autentique-se',
       username: req.body.username,
