7e590a27a9aab56c3fccd019d28cf168df53e29e
Achette <i.achette@live.com>
2024-10-23 20:29:30 -0300

    (23/10) Listagem e formulário d usuário

---
+++ vulcom/src/controllers/users.js
@@ -0,0 +1,32 @@
+import prisma from '../database/client.js'
+const controller = {}
+controller.retrieve = async function(req, res) {
+  try {
+    const users = await prisma.users.findMany()
+    res.render('users/list', {
+      title: 'Listagem de usuários',
+      users,
+      message: '',
+      error: false
+    })
+  }
+  catch(error) {
+    console.log(error)
+    res.render('users/list', {
+      title: 'Listagem de usuários',
+      users: [],
+      message: 'Erro no acesso ao banco de dados',
+      error: true
+    })
+  }
+}   
+
+controller.newUser = (req, res) => {
+    res.render('users/form', {
+        title: 'Cadastrar novo usuário',
+        message: '',
+        error: false,
+        users: {}
+    })
+}
+export default controller
