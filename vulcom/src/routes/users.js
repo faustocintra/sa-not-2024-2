6c2181b1c6e0a0baa1db8b5e7ec733b705321825
LEONARDO HENRIQUE BERNARDES DE SOUZA <1091392223034@fatec-srv.local>
2024-10-30 20:16:28 -0300

    (30/10)

--- vulcom/src/routes/users.js
+++ vulcom/src/routes/users.js
@@ -4,5 +4,7 @@ import controller from '../controllers/users.js'
 const router = Router()
 
 router.get('/', controller.retrieve)
+router.get('/new', controller.newUser)
+router.post('/', controller.upsert)
 
 export default router
