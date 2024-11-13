a1f480f2a08fecaac781dded9518284a62dce1cf
EduardoVBF <eduardo.freitas15@fatec.sp.gov.br>
2024-11-13 19:30:08 -0300

    Atualização 13.11

--- vulcom/src/routes/users.js
+++ vulcom/src/routes/users.js
@@ -4,5 +4,8 @@ import controller from '../controllers/users.js'
 const router = Router()
 
 router.get('/', controller.retrieve)
+router.get('/new', controller.newUser)
+router.get('/:id', controller.editUser)
+router.post('/', controller.upsert)
 
 export default router
