182200b4ccdbbd65e57b3293cd5dc173e5018a67
Joao Victor Moraes Vieira <88463022+jaummm1@users.noreply.github.com>
2024-11-13 19:27:44 -0300

    aula (30/10)

---
+++ vulcom/src/routes/users.js
@@ -0,0 +1,11 @@
+import { Router } from 'express'
+import controller from '../controllers/users.js'
+
+const router = Router()
+
+router.get('/', controller.retrieve)
+router.get('/new', controller.newUser)
+router.get('/:id', controller.editUser)
+router.post('/', controller.upsert)
+
+export default router
