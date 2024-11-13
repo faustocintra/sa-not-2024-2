7def94a5ba89035416814c4a83eedc4ace4c6f92
João Pedro <joaoofficialpedro@gmail.com>
2024-11-13 19:35:07 -0300

    Feat: Listagem de Usuários / Criptografia de senhas

--- vulcom/src/routes/users.js
+++ vulcom/src/routes/users.js
@@ -1,24 +1,11 @@
 import { Router } from 'express'
+import controller from '../controllers/users.js'
+
 const router = Router()
 
-/* GET all users. */
-router.get('/', function (req, res) {
-  res.send('Fetched all users')
-})
-
-/* POST a user. */
-router.post('/', function (req, res) {
-  res.send('Created a user')
-})
-
-/* PATCH a user. */
-router.patch('/', function (req, res) {
-  res.send('Updated a user')
-})
-
-/* DELETE a user. */
-router.delete('/', function (req, res) {
-  res.send('Deleted a user')
-})
+router.get('/', controller.retrieve)
+router.get('/new', controller.newUser)
+router.get('/:id', controller.editUser)
+router.post('/', controller.upsert)
 
 export default router
